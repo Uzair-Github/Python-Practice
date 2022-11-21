2645a97246520e5e7738a4626c7d0b00597b488b
Uzair-Github <siegine.893@gmail.com>
2022-11-21 13:08:12 +0500

    Merge remote-tracking branch 'origin/main'

---
+++ 21-Nov-2022/List/list.py
@@ -0,0 +1,11 @@
+list = ["apple", "banana", "cherry"]
+
+#print list as an output
+print(list)
+
+#print list's item's count as an output
+print(len(list))
+
+#print list's type as an output
+print(type(list))
+
