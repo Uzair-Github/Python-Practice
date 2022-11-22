11fd0ed3707641c4aeceb9a95ac7a9f18371627e
Uzair-Github <siegine.893@gmail.com>
2022-11-22 13:34:39 +0500

    Merge remote-tracking branch 'origin/main'

--- 20-Nov-2022/Operators/special-operators.py
+++ 20-Nov-2022/Operators/special-operators.py
@@ -1,3 +1,6 @@
+#less than, greater than and equal to application
+import math
+
 x = 15
 y = 100
 
@@ -7,3 +10,10 @@ elif x==y:
     print (x, "is equal to ", y)
 else:
     print (x, "is greater than ", y)
+
+#raise to power (2) application and underroot to power (2) application
+a = 16
+b = 2
+
+print (a,"power to", b, "is " , a**b)
+print (a,"underroot power to", b, "is " , math.sqrt(a))
