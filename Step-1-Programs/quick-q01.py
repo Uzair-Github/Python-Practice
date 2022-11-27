982aa861e5b1698236178d31f9413614bea3c188
Uzair-Github <siegine.893@gmail.com>
2022-11-27 15:31:44 +0500

    Merge remote-tracking branch 'origin/main'

---
+++ Step-1-Programs/quick-q01.py
@@ -0,0 +1,10 @@
+n = float(input("Input a number: "))
+if n >= 0:
+    if n == 0:
+        print("It is Zero")
+
+    else:
+        print("Number is Positive")
+
+else:
+    print("Number is Negative")
