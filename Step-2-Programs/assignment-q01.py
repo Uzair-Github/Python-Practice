3bced34872172b3cad6e77480794ce98c880ccb3
Uzair-Github <siegine.893@gmail.com>
2022-11-27 15:33:14 +0500

    Merge remote-tracking branch 'origin/main'

--- Step-2-Programs/assignment-q01.py
+++
@@ -1,3 +0,0 @@
-a = 20
-for a in range (0 , 20 ,2):
-    print(a)
---
+++ temp-save-01.py
@@ -0,0 +1,19 @@
+#Program have error
+star_lim = int(input("Enter a number below 10: "))
+print ("Triangle will have",star_lim,"Stars's triangle")
+x = "*"
+oput = 1
+print("Oput:",oput)
+neg_2 = 0
+print("Oput:",oput)
+space = "-"
+if star_lim<=10:
+    while oput <= star_lim:
+        neg_2 = (star_lim-2)/2
+        for neg_2
+
+        oput = oput + 1
+
+
+else:
+    print("Error")
