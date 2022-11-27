6be55d605cdc2833daf557536d0659dcb1f137a6
Uzair-Github <siegine.893@gmail.com>
2022-11-27 17:47:56 +0500

    Merge remote-tracking branch 'origin/main'

--- 27-Nov-2022/centerized-triangle.py
+++ 27-Nov-2022/centerized-triangle.py
@@ -1,28 +1,28 @@
-star_lim = int(input("Enter number: "))
-print("Your no is: ", star_lim)
-ach_lim = 0
-ch_lim = star_lim+1
-print(type(ach_lim))
+star_rows = int(input("Enter number: "))
+print("Your no is: ", star_rows)
+sym_mult = 0
+dash_mult = star_rows+1
+print(type(sym_mult))
 sym = "*"
-dash = " "
-if star_lim > 0:
-    while ach_lim <= star_lim:
-        if ach_lim/2==0:
-            #print("Printing if")
-            ach_lim += 1
-            #print("ach_lim:",ach_lim)
-            ch_lim -= 1
-            #print("ch_lim:",ch_lim)
-            print(dash * ch_lim, sym * ach_lim, dash * ch_lim)
-            #print ("dash",dash,"ch_lim",ch_lim,"sym",sym,"ach_lim",ach_lim,"dash",dash,"ch_lim",ch_lim)
+dash = "-"
+if star_rows > 0:
+    while sym_mult <= star_rows:
+        if sym_mult/2==0:
+            print("Printing if")
+            sym_mult += 1
+            #print("sym_mult:",sym_mult)
+            dash_mult -= 1
+            #print("dash_mult:",dash_mult)
+            print(dash * dash_mult, sym * sym_mult, dash * dash_mult)
+            print ("dash:",dash,"dash_mult:",dash_mult,"sym:",sym,"sym_mult:",sym_mult,"dash:",dash,"dash_mult:",dash_mult)
 
         else:
-            #print("Printing else")
-            ach_lim += 2
-            #print("ach_lim:",ach_lim)
-            ch_lim -= 1
-            #print("ch_lim:",ch_lim)
-            print(dash * ch_lim, sym * ach_lim, dash * ch_lim)
-            #print("dash", dash, "ch_lim", ch_lim, "sym", sym, "ach_lim", ach_lim, "dash", dash, "ch_lim", ch_lim)
+            print("Printing else")
+            sym_mult += 2
+            #print("sym_mult:",sym_mult)
+            dash_mult -= 1
+            #print("dash_mult:",dash_mult)
+            print(dash * dash_mult, sym * sym_mult, dash * dash_mult)
+            print ("dash:",dash,"dash_mult:",dash_mult,"sym:",sym,"sym_mult:",sym_mult,"dash:",dash,"dash_mult:",dash_mult)
 else:
     print("Error")
