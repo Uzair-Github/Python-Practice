503c5e467f20efed337b063725054741d0689699
Uzair-Github <siegine.893@gmail.com>
2022-11-27 17:47:56 +0500

    Merge remote-tracking branch 'origin/main'

--- temp-save-01.py
+++ temp-save-01.py
@@ -1,19 +1,28 @@
-#Program have error
-star_lim = int(input("Enter a number below 10: "))
-print ("Triangle will have",star_lim,"Stars's triangle")
-x = "*"
-oput = 1
-print("Oput:",oput)
-neg_2 = 0
-print("Oput:",oput)
-space = "-"
-if star_lim<=10:
-    while oput <= star_lim:
-        neg_2 = (star_lim-2)/2
-        for neg_2
+star_rows = int(input("Enter number: "))
+print("Your no is: ", star_rows)
+sym_mult = 0
+dash_mult = star_rows+1
+print(type(sym_mult))
+sym = "*"
+dash = "-"
+if star_rows > 0:
+    while sym_mult <= star_rows:
+        if sym_mult/2==0:
+            print("Printing if")
+            sym_mult += 1
+            #print("sym_mult:",sym_mult)
+            dash_mult -= 1
+            #print("dash_mult:",dash_mult)
+            print(dash * dash_mult, sym * sym_mult, dash * dash_mult)
+            print ("dash:",dash,"dash_mult:",dash_mult,"sym:",sym,"sym_mult:",sym_mult,"dash:",dash,"dash_mult:",dash_mult)
 
-        oput = oput + 1
-
-
+        else:
+            print("Printing else")
+            sym_mult += 2
+            #print("sym_mult:",sym_mult)
+            dash_mult -= 1
+            #print("dash_mult:",dash_mult)
+            print(dash * dash_mult, sym * sym_mult, dash * dash_mult)
+            print ("dash:",dash,"dash_mult:",dash_mult,"sym:",sym,"sym_mult:",sym_mult,"dash:",dash,"dash_mult:",dash_mult)
 else:
     print("Error")
