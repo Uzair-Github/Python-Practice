2a4cd678da7ab7caa0f7437c3f62088ba6fe16a9
Uzair-Github <siegine.893@gmail.com>
2022-11-27 17:10:48 +0500

    Merge remote-tracking branch 'origin/main'

--- 27-Nov-2022/centerized-triangle.py
+++ 27-Nov-2022/centerized-triangle.py
@@ -1,12 +1,28 @@
 star_lim = int(input("Enter number: "))
 print("Your no is: ", star_lim)
-ach_lim = 1
+ach_lim = 0
+ch_lim = star_lim+1
 print(type(ach_lim))
 sym = "*"
-dash = "-"
+dash = " "
 if star_lim > 0:
     while ach_lim <= star_lim:
-        print (dash)
-        ach_lim+=1
+        if ach_lim/2==0:
+            #print("Printing if")
+            ach_lim += 1
+            #print("ach_lim:",ach_lim)
+            ch_lim -= 1
+            #print("ch_lim:",ch_lim)
+            print(dash * ch_lim, sym * ach_lim, dash * ch_lim)
+            #print ("dash",dash,"ch_lim",ch_lim,"sym",sym,"ach_lim",ach_lim,"dash",dash,"ch_lim",ch_lim)
+
+        else:
+            #print("Printing else")
+            ach_lim += 2
+            #print("ach_lim:",ach_lim)
+            ch_lim -= 1
+            #print("ch_lim:",ch_lim)
+            print(dash * ch_lim, sym * ach_lim, dash * ch_lim)
+            #print("dash", dash, "ch_lim", ch_lim, "sym", sym, "ach_lim", ach_lim, "dash", dash, "ch_lim", ch_lim)
 else:
     print("Error")
