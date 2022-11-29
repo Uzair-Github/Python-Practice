b6672f3935ab20b772ba86d26f389c0dc2123114
Uzair-Github <siegine.893@gmail.com>
2022-11-29 21:04:29 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml
    #	27-Nov-2022/centerized-triangle.py

---
+++ 29-Nov-2022/finding-max-and-min.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+
+maximum = max(num1,num2,num3)
+print("Maximum number is: ",maximum)
+
+minimum = min(num1,num2,num3)
+print("Minimum number is: ",minimum)
