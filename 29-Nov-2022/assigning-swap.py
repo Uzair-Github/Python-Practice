0b74f6c64d8954c80671805e2cc3e49264e0d1a7
Uzair-Github <siegine.893@gmail.com>
2022-11-29 21:21:34 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml
    #	27-Nov-2022/centerized-triangle.py

---
+++ 29-Nov-2022/assigning-swap.py
@@ -0,0 +1,11 @@
+var1 , var2 , var3 = 3,9,81
+
+print("1st number is: ", var1)
+print("2nd number is: ", var2)
+print("3rd number is: ", var3)
+
+var1 , var2 , var3 = var3 , var2, var1
+
+print("1st number become: ", var1)
+print("2nd number become: ", var2)
+print("3rd number become: ", var3)
