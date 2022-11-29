0de67823ddb2a8985286c335b65fdeb1d8e29060
Uzair-Github <siegine.893@gmail.com>
2022-11-29 21:12:58 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml
    #	27-Nov-2022/centerized-triangle.py

---
+++ 29-Nov-2022/guess-the-flower-02.py
@@ -0,0 +1,6 @@
+flower = input("Guess a flower:")
+
+while flower !="Rose":
+    flower = input("You guessed wrong, try again: ")
+else:
+    print("You guessed right!")
---
+++ 29-Nov-2022/guess-the-flower.py
@@ -0,0 +1,8 @@
+flower = input("Guess a flower:")
+
+if flower == "Rose":
+    print ("You guessed right!")
+elif flower == "rose":
+    print("You guessed right!, capitalize your R of Rose")
+else:
+    print("You guessed wrong")
