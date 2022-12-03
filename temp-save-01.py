f3a3ec774023440233373ce692f6cefc939c8e64
Uzair-Github <siegine.893@gmail.com>
2022-12-03 22:51:33 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	.idea/workspace.xml
    #	27-Nov-2022/centerized-triangle.py

--- temp-save-01.py
+++ temp-save-01.py
@@ -1,28 +1,12 @@
-star_rows = int(input("Enter number: "))
-print("Your no is: ", star_rows)
-sym_mult = 0
-dash_mult = star_rows+1
-print(type(sym_mult))
-sym = "*"
-dash = "-"
-if star_rows > 0:
-    while sym_mult <= star_rows:
-        if sym_mult/2==0:
-            print("Printing if")
-            sym_mult += 1
-            #print("sym_mult:",sym_mult)
-            dash_mult -= 1
-            #print("dash_mult:",dash_mult)
-            print(dash * dash_mult, sym * sym_mult, dash * dash_mult)
-            print ("dash:",dash,"dash_mult:",dash_mult,"sym:",sym,"sym_mult:",sym_mult,"dash:",dash,"dash_mult:",dash_mult)
+num = int(input("Enter number: "))
+flag=False
 
-        else:
-            print("Printing else")
-            sym_mult += 2
-            #print("sym_mult:",sym_mult)
-            dash_mult -= 1
-            #print("dash_mult:",dash_mult)
-            print(dash * dash_mult, sym * sym_mult, dash * dash_mult)
-            print ("dash:",dash,"dash_mult:",dash_mult,"sym:",sym,"sym_mult:",sym_mult,"dash:",dash,"dash_mult:",dash_mult)
-else:
-    print("Error")
+if num>1:
+    for i in range (2,num):
+        if num%i==0:
+            flag=True
+            break
+    if flag == False:
+        print(num, "is prime")
+    else:
+        print(num, "is not prime")
