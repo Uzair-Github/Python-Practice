268d24826cdb3624cea6a935ff46429dd4452e56
Uzair-Github <siegine.893@gmail.com>
2023-02-06 00:23:46 +0500

    Merge remote-tracking branch 'origin/main'

--- Y23/02-February-2023/05D-02M/add-the-string-numbers.py
+++ Y23/02-February-2023/05D-02M/add-the-string-numbers.py
@@ -1,17 +1,15 @@
-str_1="3,5,7,11,14"
-print ("length of string is: ",len(str_1)-1)
+str_1 = "3,5,7,11,14"
+len_str = len(str_1)
+print("length of string is: ", len_str - 1)
 
-for i in range (0,len(str_1)):
-    if list_1[i]==",":
-        i+=1
-        if str_1[i] == int and str_1[i+1] == int:
-            #print(i)
-            temp_str=str(str_1[i])+str(str_1[i+1])
-            print(int(temp_str))
-            i+=1
-        else:
-            print("else i: ",i)
-            print("else function having list [", i, "] is", str_1[i])
-            i+=1
-    #if list_1[i] == int: #and list_1[i+1] == int:
-        #print("List i is: ",list_1[i],"List i+1 is: ",list_1[i+1])
+for i in range(0, len_str):
+    try:
+        # print(i)
+        x = int(str(str_1[i]) + str(str_1[i + 1]))
+        print("x:", x)
+
+    except ValueError:
+        # print(i)
+        if str_1[i] != ",":
+            x = int(str_1[i])
+            print("x:", x)
