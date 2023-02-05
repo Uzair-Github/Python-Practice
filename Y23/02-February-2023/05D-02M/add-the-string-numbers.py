4e80fe7677a532c2343d21035561abf8d0ae8c7e
Uzair-Github <siegine.893@gmail.com>
2023-02-06 00:33:27 +0500

    Merge remote-tracking branch 'origin/main'

--- Y23/02-February-2023/05D-02M/add-the-string-numbers.py
+++ Y23/02-February-2023/05D-02M/add-the-string-numbers.py
@@ -1,15 +1,19 @@
 str_1 = "3,5,7,11,14"
 len_str = len(str_1)
 print("length of string is: ", len_str - 1)
-
+y=0
 for i in range(0, len_str):
+    print(y)
     try:
         # print(i)
         x = int(str(str_1[i]) + str(str_1[i + 1]))
-        print("x:", x)
-
+        #print("x:", x)
+        y = x + y
+        #print(y)
     except ValueError:
         # print(i)
         if str_1[i] != ",":
             x = int(str_1[i])
-            print("x:", x)
+            #print("x:", x)
+            y=x+y
+            #print(y)
